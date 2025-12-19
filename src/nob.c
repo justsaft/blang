8a59bfefbe0f4937ede084bfccf9c22eb0f90ba7
JustSaft <69754418+justsaft@users.noreply.github.com>
2025-12-19 14:57:02 +0100

    added define for builds on macOS

--- src/nob.c
+++ src/nob.c
@@ -1,5 +1,12 @@
 
 #define _POSIX_C_SOURCE 200809L
+
+#if defined(__APPLE__)
+#define _SC_NPROCESSORS_ONLN 58
+// I have absolutely no idea why I have to define it
+// and clang can't just figure out that it should just work
+#endif
+
 #define NOB_IMPLEMENTATION
 #include "../3rd-party/nob.h"
 
